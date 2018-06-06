d24dd6b2c75ce4f6e7a724b07084751d5ad493a5
xus2 <xus2@rose-hulman.edu>
2018-06-06 11:31:25 +0800

    m2 hello world, m3 turtles, and m5 your turtles

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Siwei Xu.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,58 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow
+
+Miaow = rg.SimpleTurtle('turtle')
+
+Victoria = rg.SimpleTurtle('classic')
+
+Miaow.pen = rg.Pen('lightpink1', 5)
+
+Victoria.pen = rg.Pen('azure2', 2.5)
+
+Miaow.speed = 7
+
+Victoria.speed = 10
+
+size = 100
+
+for k in range(3):
+
+    Miaow.draw_circle(size)
+
+    Miaow.right(90)
+
+    Miaow.draw_circle(size)
+
+    Miaow.right(180)
+
+    Miaow.draw_circle(size)
+
+    Miaow.right(270)
+
+    Miaow.draw_circle(size)
+
+    Victoria.draw_square(size - 55)
+
+    Victoria.right(90)
+
+    Victoria.draw_square(size - 55)
+
+    Victoria.right(180)
+
+    Victoria.draw_square(size - 55)
+
+    Victoria.right(270)
+
+    Victoria.draw_square(size - 55)
+
+    Victoria.pen_down()
+
+    Miaow.pen_down()
+
+    size = size - 6
+
